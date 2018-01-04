2b631f77b0e51b96e11d9b47f8affb4c92b789a3
Bruce Becker <brucellino@gmail.com>
2018-01-04 08:14:53 +0100

    Add little function to use Typed.js

---
+++ app/assets/javascripts/lib/typed-page.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+var options = {
+    strings: ["Science is about applications","It's time to apply yourself." ], 
+    typeSpeed: 40
+};
+
+var typed = new Typed(".element", options);
+})
